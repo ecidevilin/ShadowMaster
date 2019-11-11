ef4e532d49b2522827604b425ee51cb2792c1228
unknown <chao.gao@corp.jingle.cn>
2019-11-11 11:35:04 +0800

    9 taps filtering

--- Assets/Pipeline/ExponentialVarianceShadowMaps/ExponentialVarianceShadowMapsPass.cs
+++ Assets/Pipeline/ExponentialVarianceShadowMaps/ExponentialVarianceShadowMapsPass.cs
@@ -15,17 +15,17 @@ namespace UnityEngine.Experimental.Rendering.LightweightPipeline.Extension
         public ComputeShader _Compute;
 
         const string _UniformFilteredMainLightSM = "_FilteredMainLightSM";
-        const string _UniformTmpMainLightSM = "_TmpMainLightSM";
+        //const string _UniformTmpMainLightSM = "_TmpMainLightSM";
 
-        const string _Compute_InputTex = "_InputTex";
+        //const string _Compute_InputTex = "_InputTex";
         const string _Compute_OutputTex = "_OutputTex";
         const string _Compute_InputTex_TexelSize = "_InputTex_TexelSize";
         const string _Compute_EVSMExponent = "_EVSMExponent";
-        const string _Compute_Vertical = "_Vertical";
+        //const string _Compute_Vertical = "_Vertical";
 
-        const string _KernelFilteringName = "KernelFiltering";
-        private int _KernelFiltering;
-        const string _KernelFirstFilteringName = "KernelFirstFiltering";
+        //const string _KernelFilteringName = "KernelFiltering";
+        //private int _KernelFiltering;
+        const string _KernelFirstFilteringName = "KernelFiltering9Taps";
         private int _KernelFirstFiltering;
         private uint _NumThreadX;
         private uint _NumThreadY;
@@ -33,14 +33,14 @@ namespace UnityEngine.Experimental.Rendering.LightweightPipeline.Extension
 
 
         RenderTargetHandle _FilteredMainLightSMHandle;
-        RenderTargetHandle _TmpMainLightSMHandle;
+        //RenderTargetHandle _TmpMainLightSMHandle;
         RenderTextureDescriptor _MainLightFilteredSMDescriptor;
         RenderTextureFormat _SMFormat;
 
         public ExponentialVarianceShadowMapsPass()
         {
             _FilteredMainLightSMHandle.Init(_UniformFilteredMainLightSM);
-            _TmpMainLightSMHandle.Init(_UniformTmpMainLightSM);
+            //_TmpMainLightSMHandle.Init(_UniformTmpMainLightSM);
         }
         public void Setup(RenderTextureDescriptor baseDescriptor, RenderTargetHandle mainLightShadowmapHandle)
         {
@@ -49,9 +49,9 @@ namespace UnityEngine.Experimental.Rendering.LightweightPipeline.Extension
                 _Enabled = false;
                 return;
             }
-            _KernelFiltering = _Compute.FindKernel(_KernelFilteringName);
+            //_KernelFiltering = _Compute.FindKernel(_KernelFilteringName);
             _KernelFirstFiltering = _Compute.FindKernel(_KernelFirstFilteringName);
-            _Compute.GetKernelThreadGroupSizes(_KernelFiltering, out _NumThreadX, out _NumThreadY, out _NumThreadZ);
+            _Compute.GetKernelThreadGroupSizes(_KernelFirstFiltering, out _NumThreadX, out _NumThreadY, out _NumThreadZ);
 
             if (_ShadowMapsPrecision == ShadowMapsPrecision.Half)
             {
@@ -78,11 +78,11 @@ namespace UnityEngine.Experimental.Rendering.LightweightPipeline.Extension
                 cmd.ReleaseTemporaryRT(_FilteredMainLightSMHandle.id);
                 _FilteredMainLightSMHandle = RenderTargetHandle.CameraTarget;
             }
-            if (_TmpMainLightSMHandle != RenderTargetHandle.CameraTarget)
-            {
-                cmd.ReleaseTemporaryRT(_TmpMainLightSMHandle.id);
-                _TmpMainLightSMHandle = RenderTargetHandle.CameraTarget;
-            }
+            //if (_TmpMainLightSMHandle != RenderTargetHandle.CameraTarget)
+            //{
+            //    cmd.ReleaseTemporaryRT(_TmpMainLightSMHandle.id);
+            //    _TmpMainLightSMHandle = RenderTargetHandle.CameraTarget;
+            //}
             CoreUtils.SetKeyword(cmd, "_EXP_VARIANCE_SHADOW_MAPS", false);
             base.FrameCleanup(cmd);
         }
@@ -126,7 +126,7 @@ namespace UnityEngine.Experimental.Rendering.LightweightPipeline.Extension
                 _MainLightFilteredSMDescriptor.autoGenerateMips = false;
                 _MainLightFilteredSMDescriptor.useMipMap = false;
             }
-            CommandBuffer cmd = CommandBufferPool.Get(_KernelFilteringName);
+            CommandBuffer cmd = CommandBufferPool.Get(_KernelFirstFilteringName);
             if (!CheckEnabled(ref renderingData))
             {
                 context.ExecuteCommandBuffer(cmd);
@@ -134,32 +134,33 @@ namespace UnityEngine.Experimental.Rendering.LightweightPipeline.Extension
                 CommandBufferPool.Release(cmd);
                 return;
             }
-            using (new ProfilingSample(cmd, _KernelFilteringName))
+            using (new ProfilingSample(cmd, _KernelFirstFilteringName))
             {
 
                 cmd.SetComputeVectorParam(_Compute, _Compute_EVSMExponent, _EVSMExponent);
                 cmd.SetComputeVectorParam(_Compute, _Compute_InputTex_TexelSize, new Vector4(1.0f / w, 1.0f / h, w, h));
                 cmd.GetTemporaryRT(_FilteredMainLightSMHandle.id, _MainLightFilteredSMDescriptor, FilterMode.Bilinear);
-                cmd.GetTemporaryRT(_TmpMainLightSMHandle.id, _MainLightFilteredSMDescriptor, FilterMode.Bilinear);
-                RenderTargetIdentifier srti = _TmpMainLightSMHandle.Identifier();
+                //cmd.GetTemporaryRT(_TmpMainLightSMHandle.id, _MainLightFilteredSMDescriptor, FilterMode.Bilinear);
+                //RenderTargetIdentifier srti = _TmpMainLightSMHandle.Identifier();
                 RenderTargetIdentifier drti = _FilteredMainLightSMHandle.Identifier();
 
-                int tgx = (int)((w + 121) / 122);
+                int tgx = (int)((w + _NumThreadX - 3) / (_NumThreadX - 2));
+                int tgy = (int)((w + _NumThreadY - 3) / (_NumThreadY - 2));
 
-                cmd.SetComputeIntParam(_Compute, _Compute_Vertical, 0);
-                cmd.SetComputeTextureParam(_Compute, _KernelFirstFiltering, _Compute_OutputTex, srti);
-                cmd.DispatchCompute(_Compute, _KernelFirstFiltering, tgx, h, 1);
-
-                context.ExecuteCommandBuffer(cmd);
-                cmd.Clear();
-
-                cmd.SetComputeTextureParam(_Compute, _KernelFiltering, _Compute_InputTex, srti);
-                cmd.SetComputeTextureParam(_Compute, _KernelFiltering, _Compute_OutputTex, drti);
-                cmd.SetComputeIntParam(_Compute, _Compute_Vertical, 1);
-                cmd.DispatchCompute(_Compute, _KernelFiltering, tgx, h, 1);
+                //cmd.SetComputeIntParam(_Compute, _Compute_Vertical, 0);
+                cmd.SetComputeTextureParam(_Compute, _KernelFirstFiltering, _Compute_OutputTex, drti);
+                cmd.DispatchCompute(_Compute, _KernelFirstFiltering, tgx, tgy, 1);
 
                 context.ExecuteCommandBuffer(cmd);
                 cmd.Clear();
+
+                //cmd.SetComputeTextureParam(_Compute, _KernelFiltering, _Compute_InputTex, srti);
+                //cmd.SetComputeTextureParam(_Compute, _KernelFiltering, _Compute_OutputTex, drti);
+                //cmd.SetComputeIntParam(_Compute, _Compute_Vertical, 1);
+                //cmd.DispatchCompute(_Compute, _KernelFiltering, tgx, h, 1);
+
+                //context.ExecuteCommandBuffer(cmd);
+                //cmd.Clear();
             }
             CoreUtils.SetKeyword(cmd, "_EXP_VARIANCE_SHADOW_MAPS", true);
             cmd.SetGlobalVector(_Compute_EVSMExponent, _EVSMExponent);
